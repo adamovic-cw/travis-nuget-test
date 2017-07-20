01fb9ff51c9cab7d763405824c5ef9b7341ff454
Slobodan Adamovic <sloba@sloba.local>
2017-07-20 17:59:14 +0200

    New attempt.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mono nuget.exe pack Travis.Nuget.Example/Travis.Nuget.Example.nuspec -Version $TEST_VERSION && \
+mono nuget.exe pack Travis.Nuget.Example/Travis.Nuget.Example.nuspec -Version $TEST_VERSION -Verbosity detailed && \
 mono nuget.exe push *.nupkg -ApiKey $NUGET_API_KEY -Verbosity detailed
