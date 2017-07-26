7bacb81d397efb50bd4ff3224dc6ef3972687337
Slobodan Adamovic <sloba@sloba.local>
2017-07-26 11:32:26 +0200

    Switch to 4.1.0 version of nuget

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mono nuget.exe pack Travis.Nuget.Example/Travis.Nuget.Example.nuspec -Version $TEST_VERSION -Verbosity detailed && \
-mono nuget.exe push *.nupkg -ApiKey $NUGET_API_KEY -Verbosity detailed
+mono nuget.exe push Travis.Nuget.Example.$TEST_VERSION.nupkg -ApiKey $NUGET_API_KEY -Verbosity detailed -Source nuget.org
